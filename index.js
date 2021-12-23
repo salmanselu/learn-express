8c1d200e3be3e447365f43adf583ab3b27ad1245
=Salman nazeer <salmanselu2002@gmail.com>
2021-12-23 13:32:13 +0530

    moved fortune cookie fuctionality to module

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'))
 
-var fortunes = [        
-    "Conquer your fears or they will conquer you.",        
-    "Rivers need springs.",        
-    "Do not fear what you don't know.",        
-    "You will have a pleasant surprise.",        
-    "Whenever possible, keep it simple.", 
-]; 
+var fortune = require('./lib/fortune');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -23,8 +17,7 @@ app.get('/', function(req, res) {
     res.render('home');
    });
 app.get('/about', function(req, res){       
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];        
-    res.render('about', { fortune: randomFortune }); 
+    res.render('about', { fortune: fortune.getFortune() }); 
 });
 
 // 404 catch-all handler (middleware)
